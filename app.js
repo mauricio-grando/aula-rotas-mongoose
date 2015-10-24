4ff584708a926581bd00d4832839cf37fbcdc2a7
Maurício Grando <mauricioa.grando@gmail.com>
2015-10-24 10:48:09 -0200

    Update app.js
    alteração para Model (foi importado o model.js)

--- app.js
+++ app.js
@@ -1,38 +1,8 @@
 var http = require('http');
+// importação do módulo model.js
+var Model = require('./model');
 
-var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/pos-unoesc');
-
-var db = mongoose.connection;
-
-db.on('error', function(err) {
-	console.log('Erro de conexao', err);
-});
-db.on('open', function() {
-	console.log('Conex aberta');
-});
-db.on('connected', function(err) {
-	console.log('Conectado');
-});
-db.on('disconnected', function(err) {
-	console.log('Desconectado');
-});
-
-var Schema = mongoose.Schema;
-
-var json_schema = {
-	name: {type: String, default: ''},
-	description: {type: String, default: ''},
-	alcohol: {type: Number, min: 0},
-	price: {type: Number, min: 0},
-	category: {type: String, default: ''},
-	created_at: {type: Date, default: Date.now}
-};
-
-var BeerSchema = new Schema(json_schema);
-
-var Beer = mongoose.model('Beer', BeerSchema);
-
+var msg = "";
 var Controller = {
 	create: function(req, res) {
 	var dados = {
@@ -43,8 +13,7 @@ var Controller = {
 			category: 'pilsen'
 		};
 
-		var model = new Beer(dados);
-		var msg = '';
+		var model = new Model(dados);
 
 		model.save(function (err, data) {
 			if(err) {
@@ -59,7 +28,7 @@ var Controller = {
 	},
 	retrieve: function(req, res) {
 		var query = {};
-		Beer.find(query, function(err, data) {
+		Model.find(query, function(err, data) {
 			if(err) {
 				console.log("Erro: ", err);
 				msg = "Erro: " + err;
@@ -78,7 +47,7 @@ var Controller = {
 			price: 6,
 			category: 'pilsen'
 		};
-		Beer.update(query, mod, function(err, data) {
+		Model.update(query, mod, function(err, data) {
 			if(err) {
 				console.log("Erro: ", err);
 				msg = "Erro: " + err;
@@ -90,8 +59,8 @@ var Controller = {
 		});
 	},
 	delete: function(req, res) {
-		var query = {name: /Skol/i};
-		Beer.remove(query, function(err, data) {
+		var query = {name: /brahma/i};
+		Model.remove(query, function(err, data) {
 			if(err) {
 				console.log("Erro: ", err);
 				msg = "Erro: " + err;
